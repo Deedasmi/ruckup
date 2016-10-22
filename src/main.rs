e5de33853f15b24c70f35b2e4f49f2d1d1d3c94a
Richard Petrie <rap1011@ksu.edu>
2016-10-22 14:13:25 -0500

    Seperate functions
    Rust is weird

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,45 @@
 extern crate rust_sodium;
 use rust_sodium::crypto::secretbox;
+use std::io::prelude::*;
+use std::fs::File;
 
 fn main() {
+    test_encrypt();
+    test_decrypt();
+
+}
+
+fn test_encrypt() {
     let key = secretbox::gen_key();
+    write_data(&key[..], "key.txt");
     let nonce = secretbox::gen_nonce();
+    write_data(&nonce[..], "nonce.txt");
     let plaintext = b"some data";
     let ciphertext = secretbox::seal(plaintext, &nonce, &key);
-    let their_plaintext = secretbox::open(&ciphertext, &nonce, &key).unwrap();
-    assert!(plaintext == &their_plaintext[..]);
+    write_data(&ciphertext[..], "cipher.txt");
+    
+    //let tp = String::from_utf8(their_plaintext).unwrap();
+    //println!("p: {:?} n: {:?} c: {:?} tp: {}", plaintext, nonce, ciphertext, tp);
+}
 
-    println!("p: {:?} n: {:?} c: {:?}", plaintext, nonce, ciphertext);
+fn test_decrypt() {
+    let k = read_data("key.txt");
+    let key = secretbox::Key::from_slice(&k[..]);
+    let n = read_data("nonce.txt");
+    let nonce = secretbox::Nonce::from_slice(&n[..]);
+    let ciphertext = read_data("cipher.txt");
+    let their_plaintext = secretbox::open(&ciphertext, &nonce.unwrap(), &key.unwrap()).unwrap();
+    println!("{}", String::from_utf8(their_plaintext).unwrap());
+}
+
+fn read_data(filename: &str) -> Vec<u8> {
+    let mut buf = Vec::new();
+    let mut f = File::open(filename).unwrap();
+    f.read_to_end(&mut buf).unwrap();
+    buf
+}
+
+fn write_data(data: &[u8], filename: &str) {
+    let mut f = File::create(filename).unwrap();
+    f.write_all(data).unwrap();
 }
