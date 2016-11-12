bfa77863f0908e658eb237dfdc058b8423ebd56a
Richard Petrie <rap1011@ksu.edu>
2016-11-11 21:15:45 -0600

    New input handling.
    Clap argument parser

--- src/bin.rs
+++ src/bin.rs
@@ -1,44 +1,83 @@
 extern crate rust_sodium;
 extern crate preferences;
 extern crate rustc_serialize;
-use preferences::{AppInfo, PreferencesMap, Preferences, PreferencesError};
+extern crate app_dirs;
+#[macro_use]
+extern crate clap;
+use preferences::{AppInfo, PreferencesMap, Preferences};
 use rust_sodium::crypto::secretbox;
-use rustc_serialize::{json, Encodable};
-use std::io;
+use rustc_serialize::json;
+use clap::App;
+use app_dirs::{app_dir, AppDataType};
+use std::path::PathBuf;
 
 
-static PREFLOC: &'static str = "ruckup";
+const PREFLOC: &'static str = "preferences/ruckup";
 const APP_INFO: AppInfo = AppInfo {
     name: "ruckup",
     author: "ruckup",
 };
 
+#[allow(unused_variables)]
 fn main() {
+    let meta_loc = app_dir(AppDataType::UserCache, &APP_INFO, "metadata/meta").unwrap();
+    println!("Welcome to Ruckup! Loading settings...");
+
+    // Parse cli arguments
+    let yaml = load_yaml!("cli.yml");
+    let matches = App::from_yaml(yaml).get_matches();
+
     // Load preferences
-    let prefmap = PreferencesMap::<String>::load(&APP_INFO, &PREFLOC)
+    let mut prefmap = PreferencesMap::<String>::load(&APP_INFO, &PREFLOC)
         .unwrap_or(PreferencesMap::<String>::new());
     // Load key
-    let key: secretbox::Key = if prefmap.contains_key("key".into()) {
-        println!("Loading key");
-        let k = prefmap.get("key".into()).unwrap();
-        json::decode(k).unwrap()
-    } else {
-        println!("No key found! Assumed intentional");
-        let k = secretbox::gen_key();
-        insert_pref(prefmap, "key".into(), &k).unwrap();
-        k
-    };
+    println!("Loading key");
+    let key: secretbox::Key = prefmap.get("key".into())
+        .and_then(|x| json::decode(x).ok())
+        .unwrap_or_else(|| {
+            println!("No key found! Assumed intentional");
+            let k = secretbox::gen_key();
+            prefmap.insert("key".into(), json::encode(&k).unwrap());
+            k
+        });
     println!("Found key {:?}", key);
-    loop {
-        match input().trim() {
-            "help" | "h" | "?" => println!("I should really add a help section"),
-            "quit" | "q" => {
-                println!("Goodbye!");
-                break;
-            }
-            _ => println!("Not implemented!"),
-        };
+
+    // Load src folders
+    let mut src_locs: Vec<PathBuf> =
+        prefmap.get("src_locs".into()).and_then(|x| json::decode(x).ok()).unwrap_or_else(|| {
+            println!("No src locations found! Adding metadata table to backup locations.");
+            prefmap.insert("src_locs".into(), json::encode(&meta_loc).unwrap());
+            vec![meta_loc]
+        });
+
+    // Parse -ts
+    if let Some(ts) = matches.value_of("temporary_storage") {
+        // TODO Verify path
+        prefmap.insert("temp_store".into(), json::encode(&ts).unwrap());
+        println!("Set temporary storage to {}", ts);
     }
+
+    // Parse -s
+    if let Some(s) = matches.value_of("src_loc") {
+        // TODO Verify path
+        src_locs.push(PathBuf::from(s));
+        prefmap.insert("src_locs".into(), json::encode(&src_locs).unwrap());
+        println!("Added {} to backup locations!", s);
+    }
+    // let t: Vec<PathBuf> =
+    // prefmap.get("src_locs".into()).and_then(|x| json::decode(x).unwrap()).unwrap();
+    // println!("Source locations: {:?}", t);
+
+    // Build walkdir iterator
+
+    // Load storage location
+    let temp_store: String = prefmap.get("temp_store".into())
+        .and_then(|x| json::decode(x).ok())
+        .expect("Need a temporary storage location! Set with ruckup -t <path>");
+
+    // Save preferences
+    prefmap.save(&APP_INFO, &PREFLOC).expect("Failed to save preferences!");
+    println!("Preferences saved! Goodbye!");
 
 
     // Gather files
@@ -50,19 +89,4 @@ fn main() {
     // Decrypt files
     // println!("Decrypting file");
     // lib::decrypt(key, nonce, "cipher.txt", "output.txt");
-}
-
-fn input() -> String {
-    let mut input = String::new();
-    println!("Please enter a command");
-    io::stdin().read_line(&mut input).unwrap();
-    input.to_lowercase()
-}
-
-fn insert_pref<T: Encodable>(mut prefs: PreferencesMap,
-                             key: String,
-                             value: T)
-                             -> Result<(), PreferencesError> {
-    prefs.insert(key.into(), json::encode(&value).unwrap());
-    prefs.save(&APP_INFO, &PREFLOC)
 }
