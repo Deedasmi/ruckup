b2df294c67415df7450d1b1f69430e3871acbf7c
Richard Petrie <rap1011@ksu.edu>
2016-11-12 23:45:03 -0600

    Closes #2

--- src/bin.rs
+++ src/bin.rs
@@ -4,6 +4,9 @@ extern crate rustc_serialize;
 extern crate app_dirs;
 extern crate lib;
 #[macro_use]
+extern crate log;
+extern crate log4rs;
+#[macro_use]
 extern crate clap;
 use preferences::{AppInfo, PreferencesMap, Preferences};
 use rust_sodium::crypto::secretbox;
@@ -20,13 +23,18 @@ const APP_INFO: AppInfo = AppInfo {
 
 #[allow(unused_variables)]
 fn main() {
-    let meta_loc = app_dir(AppDataType::UserCache, &APP_INFO, "metadata/meta").unwrap();
     println!("Welcome to Ruckup! Loading settings...");
+    log4rs::init_file("log_config.yml", Default::default()).unwrap();
+    debug!("Logger loaded");
+    let meta_loc = app_dir(AppDataType::UserCache, &APP_INFO, "metadata/meta").unwrap();
+    debug!("Meta_loc set {:?}", meta_loc);
+
 
     // Parse cli arguments
     let yaml = load_yaml!("cli.yml");
     let matches = App::from_yaml(yaml).get_matches();
 
+    // TODO Have this effect logging level
     // Set verbosity level
     let v = matches.occurrences_of("verbose");
 
@@ -34,29 +42,22 @@ fn main() {
     let mut prefmap = PreferencesMap::<String>::load(&APP_INFO, &PREFLOC)
         .unwrap_or(PreferencesMap::<String>::new());
     // Load key
-    if v > 0 {
-        println!("Loading key");
-    }
+    debug!("Loading key");
+
     let key: secretbox::Key = prefmap.get("key".into())
         .and_then(|x| json::decode(x).ok())
         .unwrap_or_else(|| {
-            if v > 0 {
-                println!("No key found! Assumed intentional");
-            }
+            warn!("No key found! Assumed intentional");
             let k = secretbox::gen_key();
             prefmap.insert("key".into(), json::encode(&k).unwrap());
             k
         });
-    if v > 1 {
-        println!("Found key {:?}", key);
-    }
+    debug!("Found key {:?}", key);
 
     // Load src folders
     let mut src_locs: Vec<PathBuf> =
         prefmap.get("src_locs".into()).and_then(|x| json::decode(x).ok()).unwrap_or_else(|| {
-            if v > 0 {
-                println!("No src locations found! Adding metadata table to backup locations.");
-            }
+            warn!("No src locations found! Adding metadata table to backup locations.");
             prefmap.insert("src_locs".into(), json::encode(&meta_loc).unwrap());
             vec![meta_loc]
         });
@@ -78,13 +79,11 @@ fn main() {
         println!("Added {} to backup locations!", s);
     }
 
-    // let t: Vec<PathBuf> =
-    // prefmap.get("src_locs".into()).and_then(|x| json::decode(x).unwrap()).unwrap();
-    // println!("Source locations: {:?}", t);
+    debug!("Source locations: {:?}", src_locs);
 
     // Build walkdir iterator
     let all_files = lib::get_file_vector(src_locs);
-    println!("{:?}", all_files);
+    info!("{} files found", all_files.into_iter().count());
 
     // Load storage location
     let temp_store: String = prefmap.get("temp_store".into())
@@ -93,10 +92,6 @@ fn main() {
 
     // Save preferences
     prefmap.save(&APP_INFO, &PREFLOC).expect("Failed to save preferences!");
-    if v == 0 {
-        println!("Goodbye!")
-    } else {
-        println!("Preferences saved! Goodbye!");
-    }
+    println!("Preferences saved! Goodbye!");
 
 }
